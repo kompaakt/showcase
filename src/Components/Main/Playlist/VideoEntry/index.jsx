40f2c9d3c042e0af8191091c82458ff40f3186b2
dmitriys9701 <dmitriys9701@yandex.ru>
2019-04-11 00:38:48 +0300

    add VideoEntry Component

---
+++ src/Components/Main/Playlist/VideoEntry/index.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import YOUTUBE_API from "../../../../config";
+import "./index.css";
+
+export default class VideoEntry extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: props.id,
+      name: null,
+      icon: null,
+      info: null
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      `https://www.googleapis.com/youtube/v3/videos?id=${
+        this.state.id
+      }&part=snippet&key=${YOUTUBE_API}`
+    )
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({ info: data.items[0] });
+      });
+  }
+
+  handleClickRemove = () => {
+    this.props.handleRemove(this.state.id);
+  };
+
+  handleClickPlay = () => {
+    this.props.handlePlay(this.state.id);
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.info ? (
+          <div className="root">
+            <img
+              src={this.state.info.snippet.thumbnails.default.url}
+              alt={this.state.info.snippet.title}
+            />
+            <b>{this.state.info.snippet.title}</b>
+            <div className="controlButtons">
+              <button
+                className="playVideoButton"
+                onClick={this.handleClickPlay}
+              >
+                <span role="img">Play</span>
+              </button>
+              <button
+                className="removeVideoButton"
+                onClick={this.handleClickRemove}
+              >
+                <span role="img">X</span>
+              </button>
+            </div>
+          </div>
+        ) : (
+          this.state.id
+        )}
+      </React.Fragment>
+    );
+  }
+}
