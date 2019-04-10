176b6e7a7ca99f8a9431fe8f1a14cd3082debdd5
dmitriys9701 <dmitriys9701@yandex.ru>
2019-04-11 00:38:32 +0300

    add Playlist Component

---
+++ src/Components/Main/Playlist/index.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import VideoEntry from "./VideoEntry/index";
+import MD5 from "object-hash";
+
+class Playlist extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [{ id: "2g811Eo7K8U", timeAdded: new Date() }],
+      newVideoFromInput: null
+    };
+  }
+
+  handleClick = () => {
+    const inputValue = this.state.newVideoFromInput;
+    let sanitizedVideoId = null;
+    const match = inputValue.match(
+      /^.*(youtu\.be\/|vi?\/|u\/\w\/|embed\/|\?vi?=|\&vi?=)([^#\&\?]*).*/
+    );
+    if (match && match[2].length === 11) {
+      sanitizedVideoId = match[2];
+      const newVideos = this.state.videos;
+      newVideos.push({ id: sanitizedVideoId, timeAdded: new Date() });
+      this.setState({ videos: newVideos });
+    } else {
+      alert("not a valid video id");
+    }
+  };
+
+  handleChange = event => {
+    this.setState({ newVideoFromInput: event.target.value });
+  };
+
+  handleRemoveVideo = id => {
+    const newVideos = this.state.videos.filter(video => video.id !== id);
+    this.setState({ videos: newVideos });
+  };
+
+  handlePlayVideo = id => {
+    this.props.handlePlayVideo(id);
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <input type="text" onChange={this.handleChange} />
+          <button onClick={this.handleClick}>+</button>
+        </div>
+        <ol>
+          {this.state.videos.map(video => (
+            <li key={MD5(video)}>
+              {
+                <VideoEntry
+                  id={video.id}
+                  handleRemove={this.handleRemoveVideo}
+                  handlePlay={this.handlePlayVideo}
+                />
+              }
+            </li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
+}
+
+export default Playlist;
