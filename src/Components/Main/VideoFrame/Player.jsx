323c0b8e69ebf7f4b39a6377a8114bc10d17c7d3
dmitriys9701 <dmitriys9701@yandex.ru>
2019-04-16 00:09:48 +0300

    add classname + styles

--- src/Components/Main/VideoFrame/Player.jsx
+++ src/Components/Main/VideoFrame/Player.jsx
@@ -1,37 +1,33 @@
 import React from "react";
 import YouTube from "react-youtube";
+import "./index.css";
 
 class Player extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      player: null
-    };
-  }
-
   _onReady = event => {
     this.props.setPlayer(event.target);
     event.target.pauseVideo();
   };
 
   render() {
+    const size = this.props.size;
     const opts = {
-      height: "360",
-      width: "720",
+      height: size.height,
+      width: size.width,
       playerVars: {
         // https://developers.google.com/youtube/player_parameters
         autoplay: 1
       }
     };
-
     return (
-      <YouTube
-        videoId={this.props.videoId ? this.props.videoId : "2g811Eo7K8U"}
-        opts={opts}
-        onReady={this._onReady}
-        onStateChange={this.props.handleSendPlayingStatus}
-      />
+      <>
+        <YouTube
+          videoId={this.props.videoId ? this.props.videoId : "2g811Eo7K8U"}
+          opts={opts}
+          onReady={this._onReady}
+          className={"YouTubeIFrame"}
+          onStateChange={this.props.handleSetPlayingStatus}
+        />
+      </>
     );
   }
 }
