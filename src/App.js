a9c797f17021a2bc2375d87393fb9759183f3d4b
dmitriys9701 <dmitriys9701@yandex.ru>
2019-04-16 18:30:37 +0300

    add theming

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import "./App.css";
 import WelcomeScreen from "./components/WelcomeScreen/index.jsx";
 // import MainScreen from "./components/Main/index";
 import Room from "./components/Room/index.jsx";
+import { ThemeProvider } from "styled-components";
+import Theme from "./theme";
 
 class App extends Component {
   render() {
     return (
-      <Router className="App">
-        <Route path={"/"} exact={true} component={WelcomeScreen} />
-        <Route path={`/room/:roomId`} component={Room} />
-      </Router>
+      <ThemeProvider theme={Theme}>
+        <Router className="App">
+          <Route path={"/"} exact={true} component={WelcomeScreen} />
+          <Route path={`/room/:roomId`} component={Room} />{" "}
+        </Router>
+      </ThemeProvider>
     );
   }
 }
---
+++ src/theme.js
@@ -0,0 +1,12 @@
+const Theme = {
+  gradient: `linear-gradient(
+    135deg,
+    rgba(74, 66, 118, 1) 0%,
+    rgba(74, 66, 118, 1) 2%,
+    rgba(194, 114, 102, 1) 18%,
+    rgba(50, 143, 125, 1) 46%,
+    rgba(83, 83, 193, 1) 80%,
+    rgba(224, 94, 77, 1) 100%
+  )`
+};
+export default Theme;
