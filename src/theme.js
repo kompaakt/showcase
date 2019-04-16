8d0f7f4c94720d2372762602663d353f6681400c
dmitriys9701 <dmitriys9701@yandex.ru>
2019-04-16 19:05:51 +0300

    add button theming

--- src/theme.js
+++ src/theme.js
@@ -7,6 +7,7 @@ const Theme = {
     rgba(50, 143, 125, 1) 46%,
     rgba(83, 83, 193, 1) 80%,
     rgba(224, 94, 77, 1) 100%
-  )`
+  )`,
+  button: `linear-gradient(200deg, #0298e2, #ffeb3b);`
 };
 export default Theme;
