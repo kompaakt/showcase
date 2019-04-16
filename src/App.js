7a0c0eec8cd6525957dfa10c9ec1eed26375b817
dmitriys9701 <dmitriys9701@yandex.ru>
2019-04-16 19:48:55 +0300

    add global styemle + ability to switch themes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.css";
 import WelcomeScreen from "./components/WelcomeScreen/index.jsx";
-// import MainScreen from "./components/Main/index";
+import { createGlobalStyle, ThemeProvider } from "styled-components/macro";
 import Room from "./components/Room/index.jsx";
-import { ThemeProvider } from "styled-components/macro";
 import Theme from "./theme";
 
-class App extends Component {
-  render() {
-    return (
-      <ThemeProvider theme={Theme}>
+const GlobalStyle = createGlobalStyle`
+  html {
+    background: ${props => props.theme.gradient};
+    background-size: 100% auto;
+    height: 100%;
+    width: 100%;
+  }
+`;
+const App = props => {
+  const [theme, setTheme] = useState({ name: "light", theme: Theme.light });
+
+  const switchTheme = () => {
+    switch (theme.name) {
+      case "light": {
+        setTheme({ name: "dark", theme: Theme.dark });
+        break;
+      }
+      case "dark": {
+        setTheme({ name: "light", theme: Theme.light });
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  };
+
+  return (
+    <ThemeProvider theme={theme.theme}>
+      <>
+        <GlobalStyle />
         <Router className="App">
           <Route path={"/"} exact={true} component={WelcomeScreen} />
-          <Route path={`/room/:roomId`} component={Room} />{" "}
+          <Route
+            path={`/room/:roomId`}
+            component={props => <Room {...props} switchTheme={switchTheme} />}
+          />
         </Router>
-      </ThemeProvider>
-    );
-  }
-}
+      </>
+    </ThemeProvider>
+  );
+};
 
 export default App;
--- src/theme.js
+++ src/theme.js
@@ -1,22 +1,45 @@
 const Theme = {
-  gradient: `linear-gradient(
-    135deg,
-    rgba(74, 66, 118, 1) 0%,
-    rgba(74, 66, 118, 1) 2%,
-    rgba(194, 114, 102, 1) 18%,
-    rgba(50, 143, 125, 1) 46%,
-    rgba(83, 83, 193, 1) 80%,
-    rgba(224, 94, 77, 1) 100%
-  )`,
-  button: {
-    color: `linear-gradient(200deg, #0298e2, #ffeb3b)`,
-    size: `400% 400%`
-  },
-  font: {
-    size: {
-      main: `6vw`
+  light: {
+    gradient: `linear-gradient(
+      135deg,
+      rgba(74, 66, 118, 1) 0%,
+      rgba(74, 66, 118, 1) 2%,
+      rgba(194, 114, 102, 1) 18%,
+      rgba(50, 143, 125, 1) 46%,
+      rgba(83, 83, 193, 1) 80%,
+      rgba(224, 94, 77, 1) 100%
+    )`,
+    button: {
+      color: `linear-gradient(200deg, #0298e2, #ffeb3b)`,
+      size: `400% 400%`
     },
-    name: `Fredoka One`
+    font: {
+      size: {
+        main: `6vw`
+      },
+      name: `Fredoka One`
+    }
+  },
+  dark: {
+    gradient: `linear-gradient(
+      135deg,
+      rgba(224, 94, 77, 1) 0%,
+      rgba(83, 83, 193, 1) 2%,
+      rgba(50, 143, 125, 1) 18%,
+      rgba(194, 114, 102, 1) 46%,
+      rgba(74, 66, 118, 1) 80%,
+      rgba(74, 66, 118, 1) 100%
+    )`,
+    button: {
+      color: `linear-gradient(200deg, #ffeb3b, #0298e2)`,
+      size: `400% 400%`
+    },
+    font: {
+      size: {
+        main: `6vw`
+      },
+      name: `Fredoka One`
+    }
   }
 };
 export default Theme;
