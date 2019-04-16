fcc142b9df10bc11abd6a99ae02cb7f6d5f1cc4e
dmitriys9701 <dmitriys9701@yandex.ru>
2019-04-16 17:04:25 +0300

    rename dir

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
-import WelcomeScreen from "./Components/WelcomeScreen/index.jsx";
-import MainScreen from "./Components/Main/index";
-import Room from "./Components/Room/index.jsx";
+import WelcomeScreen from "./components/WelcomeScreen/index.jsx";
+// import MainScreen from "./components/Main/index";
+import Room from "./components/Room/index.jsx";
 
 class App extends Component {
   render() {
