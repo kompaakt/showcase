a510959e37bae5a5357be54f8dbd6d502914aa74
dmitriys9701 <dmitriys9701@yandex.ru>
2019-04-21 14:03:34 +0300

    add type definitions

---
+++ src/types/YouTubeIFrameStateEnum.ts
@@ -0,0 +1,10 @@
+enum IFrameState {
+  NOT_STARTED = -1,
+  END = 0,
+  PLAY = 1,
+  PAUSE = 2,
+  BUFFERING = 3,
+  QUEUE = 5
+}
+
+export default IFrameState;
---
+++ src/types/react-player.d.ts
@@ -0,0 +1,1 @@
+declare module "react-player/lib/players/YouTube";
